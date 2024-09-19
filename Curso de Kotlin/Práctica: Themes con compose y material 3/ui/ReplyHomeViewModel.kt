57b0eeca0c627c8d0e183546a6030763275f6d5d
AlfredoQS151 <98185896+AlfredoQS151@users.noreply.github.com>
2024-09-19 14:21:03 -0600

    Update ReplyHomeViewModel.kt

--- Curso de Kotlin/Práctica: Themes con compose y material 3/ui/ReplyHomeViewModel.kt
+++ Curso de Kotlin/Práctica: Themes con compose y material 3/ui/ReplyHomeViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2022 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.reply.ui
 
 import androidx.lifecycle.ViewModel
@@ -28,7 +12,6 @@ import kotlinx.coroutines.launch
 
 class ReplyHomeViewModel : ViewModel() {
 
-    // UI state exposed to the UI
     private val _uiState = MutableStateFlow(ReplyHomeUIState(loading = true))
     val uiState: StateFlow<ReplyHomeUIState> = _uiState.asStateFlow()
 
@@ -45,9 +28,7 @@ class ReplyHomeViewModel : ViewModel() {
     }
 
     fun setSelectedEmail(emailId: Long) {
-        /**
-         * We only set isDetailOnlyOpen to true when it's only single pane layout
-         */
+
         val email = uiState.value.emails.find { it.id == emailId }
         _uiState.value = _uiState.value.copy(
             selectedEmail = email,
